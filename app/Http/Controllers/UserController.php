03926c1fa5d5f1d30990ad1c00297e74ab8d8b32
nnivxix <adhe.hanasa@gmail.com>
2023-08-17 17:31:51 +0700

    feat: add controller login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserRegisterRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
@@ -16,11 +18,31 @@ class UserController extends Controller
         $validated = $request->validated();
 
         $user = new User($validated);
-
-
         $user->password = Hash::make($validated['password']);
         $user->save();
 
         return (new UserResource($user))->response()->setStatusCode(201);
+    }
+
+    public function login(UserLoginRequest $request): UserResource
+    {
+        $validated = $request->validated();
+
+        $user = User::where('username', $validated['username'])->first();
+
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            throw new HttpResponseException(response([
+                "errors" => [
+                    "message" => [
+                        "username or password wrong"
+                    ]
+                ]
+            ], 401));
+        }
+
+        $user->token = Str::uuid()->toString();
+        $user->save();
+
+        return new UserResource($user);
     }
 }
