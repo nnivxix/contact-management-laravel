ea0f7c19ba87a2c84305c7095c77842c63f32d0f
nnivxix <adhe.hanasa@gmail.com>
2023-08-18 21:24:28 +0700

    feat: add middleware ApiAuth

--- app/Http/Middleware/ApiAuth.php
+++ app/Http/Middleware/ApiAuth.php
@@ -3,18 +3,41 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class ApiAuth
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        $token = $request->header('Authorization');
+        $authenticate = true;
+
+        // if token is null
+        if (!$token) {
+            $authenticate = false;
+        }
+
+        $user = User::where('token', $token)->first();
+
+        // if user not found
+        if (!$user) {
+            $authenticate = false;
+        } else {
+            Auth::login($user);
+        }
+
+        if ($authenticate) {
+            return $next($request);
+        }
+        return response()->json([
+            "errors" => [
+                "message" => [
+                    "unauthorized"
+                ]
+            ]
+        ])->setStatusCode(401);
     }
 }
