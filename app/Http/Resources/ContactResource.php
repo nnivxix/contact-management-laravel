114e001a5ab3f98510a3a91659c7bb6c5ddb0074
nnivxix <adhe.hanasa@gmail.com>
2023-08-19 10:56:22 +0700

    feat(contact) : add ContactResource

---
+++ app/Http/Resources/ContactResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ContactResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'first_name' => $this->first_name,
+            'last_name'  => $this->last_name,
+            'email'      => $this->email,
+            'phone'      => $this->phone,
+        ];
+    }
+}
