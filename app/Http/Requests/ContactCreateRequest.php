5f50357b41d91d44e2654bb8bddb9709c2890652
nnivxix <adhe.hanasa@gmail.com>
2023-08-19 10:54:28 +0700

    feat(contact) : add ContactCreateResquest

---
+++ app/Http/Requests/ContactCreateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ContactCreateRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return $this->user() != null;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'first_name' => ['required', 'max:100'],
+            'last_name'  => ['nullable'],
+            'email'      => ['nullable', 'email'],
+            'phone'      => ['required', 'max:100'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response([
+            "errors" => $validator->getMessageBag()
+        ], 400));
+    }
+}
--- database/migrations/2023_08_17_081553_create_contacts_table.php
+++ database/migrations/2023_08_17_081553_create_contacts_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email');
+            $table->string('last_name')->nullable();
+            $table->string('email')->nullable();
             $table->string('phone');
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
