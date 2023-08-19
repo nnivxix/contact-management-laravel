3095e955f7cadb33288efbe74dcd7af7d15eabf7
nnivxix <adhe.hanasa@gmail.com>
2023-08-19 17:57:20 +0700

    feat(contact): show contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ContactCreateRequest;
-use App\Http\Resources\ContactResource;
 use App\Models\Contact;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\ContactResource;
+use App\Http\Requests\ContactCreateRequest;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ContactController extends Controller
 {
@@ -25,6 +26,22 @@ class ContactController extends Controller
         return (new ContactResource($contact))->response()->setStatusCode(201);
     }
 
+    public function show(string $id)
+    {
+        $contact = Contact::query()->whereId($id);
+
+        if (!$contact) {
+            throw new HttpResponseException(response()->json([
+                'errors' => [
+                    "message" => [
+                        "not found"
+                    ]
+                ]
+            ])->setStatusCode(404));
+        }
+
+        return new ContactResource($contact);
+    }
 
     public function update(Request $request, string $id)
     {
