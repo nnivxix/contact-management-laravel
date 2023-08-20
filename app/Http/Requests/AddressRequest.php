0862d462e38c47ca6920e3632b6865e33cba1cb6
nnivxix <adhe.hanasa@gmail.com>
2023-08-20 07:59:42 +0700

    feat(address): request

--- app/Http/Requests/AddressRequest.php
+++ app/Http/Requests/AddressRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class AddressRequest extends FormRequest
 {
@@ -19,5 +21,12 @@ class AddressRequest extends FormRequest
             'city'    => ['nullable'],
             'country' => ['required'],
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response([
+            "errors" => $validator->getMessageBag()
+        ], 400));
     }
 }
