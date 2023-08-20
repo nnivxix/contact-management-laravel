cfc161c3ae95c8111e6df5d91c66c2a09183d0ac
nnivxix <adhe.hanasa@gmail.com>
2023-08-20 17:49:38 +0700

    feat: address seeder

--- database/seeders/AddressSeeder.php
+++ database/seeders/AddressSeeder.php
@@ -2,16 +2,21 @@
 
 namespace Database\Seeders;
 
+use App\Models\Address;
+use App\Models\Contact;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class AddressSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $contact = Contact::query()->limit(1)->first();
+
+        Address::create([
+            'city'       => 'test',
+            'country'    => 'test',
+            'contact_id' => $contact->id,
+        ]);
     }
 }
